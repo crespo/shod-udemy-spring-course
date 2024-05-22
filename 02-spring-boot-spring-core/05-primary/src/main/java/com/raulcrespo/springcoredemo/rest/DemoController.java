651584cf3b1730e96ae0e94341fe7b02f5ea9fe0
raul crespo <raulxcrespo@gmail.com>
2024-05-22 06:40:13 -0300

    finish primary example

---
+++ 02-spring-boot-spring-core/05-primary/src/main/java/com/raulcrespo/springcoredemo/rest/DemoController.java
@@ -0,0 +1,23 @@
+package com.raulcrespo.springcoredemo.rest;
+
+import com.raulcrespo.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    // define private field for the dependency
+    private Coach myCoach;
+
+    // constructor dependency injection
+    @Autowired
+    public DemoController(Coach myCoach) {
+        this.myCoach = myCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout() {
+        return myCoach.getDailyWorkout();
+    }
+}
